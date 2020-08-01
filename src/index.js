7eaed2f42aa3829d78480efc0704ca6261b79713
kamimura4035 <39050957+kamimura4035@users.noreply.github.com>
2020-08-01 18:14:27 +0900

    check機能追加

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { TodoListModel, TodoModel } from "./model";
 
 const input = document.getElementById("input");
 const todoListView = document.getElementById("todoList");
-
 const todoListModel = new TodoListModel();
 
 input.addEventListener("keyup", event => {
@@ -14,7 +13,6 @@ input.addEventListener("keyup", event => {
     todoListModel.add(todo);
     todoListRender(todoListModel);
 
-    // xボタンのイベント設置
     event.target.value = "";
   }
 });
@@ -23,13 +21,30 @@ const todoListRender = todoListModel => {
   const ul = element`<ul></ul>`;
   todoListModel.todos.forEach(todo => {
     const li = element`
-    <li id="${todo.id}">${todo.name} x</li>      
+    <li id="${todo.id}"><input type="checkbox">${
+      todo.name
+    } <span>x</span></li>      
     `;
-    li.addEventListener("click", event => {
-      const todoId = event.target.id;
+
+    // x削除ボタンにイベントを紐つける
+    const span = li.getElementsByTagName("span")[0];
+    span.addEventListener("click", event => {
+      const todoId = event.target.parentNode.id;
       todoListModel.remove(todoId);
       todoListRemoveRender(todoId);
+
+      // 数を表示しているUIを表示する
     });
+
+    // checkboxにイベントをひもつける
+    const input = li.getElementsByTagName("input")[0];
+    input.addEventListener("change", event => {
+      const todoId = event.target.parentNode.id;
+      const checked = event.target.checked;
+      todoListModel.toggleChecked(todoId, checked);
+      todoListCheckedRender(todoId, checked);
+    });
+
     ul.appendChild(li);
   });
   todoListView.innerHTML = "";
@@ -39,4 +54,13 @@ const todoListRender = todoListModel => {
 const todoListRemoveRender = todoId => {
   const todo = document.getElementById(todoId);
   todo.remove();
+};
+
+const todoListCheckedRender = (todoId, checked) => {
+  const todo = document.getElementById(todoId);
+  if (checked) {
+    todo.className = "checked";
+  } else {
+    todo.className = "";
+  }
 };
--- src/model.js
+++ src/model.js
@@ -9,11 +9,20 @@ export class TodoListModel {
   remove(id) {
     this.todos = this.todos.filter(todo => todo.id !== Number(id));
   }
+  toggleChecked(id, checked) {
+    this.todos = this.todos.map(todo => {
+      if (todo.id === id) {
+        todo.checked = checked;
+      }
+      return todo;
+    });
+  }
 }
 
 export class TodoModel {
   constructor(name) {
     this.id = new Date().getTime();
     this.name = name;
+    this.checked = false;
   }
 }
