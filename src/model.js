9643b1e4f4568b50f433d79525db6d6554b7224f
kamimura4035 <39050957+kamimura4035@users.noreply.github.com>
2020-08-01 23:43:56 +0900

    event emitterの継承

--- src/model.js
+++ src/model.js
@@ -1,13 +1,24 @@
+import { EventEmitter } from "./event";
+
 // model導入
-export class TodoListModel {
+export class TodoListModel extends EventEmitter {
   constructor() {
+    super();
     this.todos = [];
+  }
+  getCheckedTodosCount() {
+    return this.todos.filter(todo => todo.checked === true).length;
+  }
+  onChange(listener) {
+    this.addEventListener("change", listener);
   }
   add(todo) {
     this.todos.push(todo);
+    this.emit("change");
   }
   remove(id) {
     this.todos = this.todos.filter(todo => todo.id !== Number(id));
+    this.emit("change");
   }
   toggleChecked(id, checked) {
     this.todos = this.todos.map(todo => {
@@ -16,9 +27,7 @@ export class TodoListModel {
       }
       return todo;
     });
-  }
-  getCheckedTodosCount() {
-    return this.todos.filter(todo => todo.checked === true).length;
+    this.emit("change");
   }
 }
 
